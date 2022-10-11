0eef038252d647b9757e06a3faebc69796affdc1
olivercv <oliver_c_v@hotmail.com>
2022-10-10 21:55:38 -0400

    WIP sign up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) { 
     this.form = this.fb.group({
       email: ['olivercv@gmail.com', [Validators.required, Validators.email]],
       password: ['123456', [Validators.required, Validators.minLength(6)]]
@@ -18,11 +20,29 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  login() {
+  async login() {
     console.log(this.form.value);
-  }
-  register() {
+    const { user, session, error} = await this.auth.login(this.form.value);    
     
+    if(error) {
+      // TODO show error list
+      console.log(error);
+    }else {
+      this.router.navigateByUrl('/app', { replaceUrl: true});
+    }
+  }
+  async register() {
+    console.log(this.form.value);
+    const { user, session, error} = await this.auth.createAccount(this.form.value);
+    
+    if(error) {
+      // TODO show error list
+      console.log(error);
+    }else {
+      console.log(session);
+      console.log(user);
+      this.router.navigateByUrl('/app', { replaceUrl: true});
+    }
   }
 
 }
